5567d5f42aa2dadfc6ca40f5518bec7e61bd16f1
Ali-Maia <aliciaengcomp@gmail.com>
2026-05-28 17:13:28 -0300

    feat: set up React Router v6 with all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Portfolio from './Portfolio';
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
 
 function App() {
-  return (
-    <Portfolio />
-  )
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
---
+++ src/router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import ProjectsList from './pages/ProjectsList'
+import ProjectDetail from './pages/ProjectDetail'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true,               element: <Home /> },
+      { path: 'projetos',          element: <ProjectsList /> },
+      { path: 'projetos/:slug',    element: <ProjectDetail /> },
+      { path: 'sobre',             element: <About /> },
+      { path: 'contato',           element: <Contact /> },
+    ],
+  },
+])
+
+export default router
