e671813c23991107260eab401a34502414dc9672
Ali-Maia <aliciaengcomp@gmail.com>
2026-05-28 22:20:43 -0300

    feat: add Test Adopet and Organo Test Robot projects
    - test-adopet: Cypress E2E suite for animal adoption platform with GitHub Actions CI
    - organo-test-robot: Robot Framework suite for Organo team organizer app using Page Object pattern

--- src/data/projects/index.js
+++ src/data/projects/index.js
@@ -2,7 +2,9 @@ import estoqueTeste from './estoque-teste.json'
 import bookcart from './bookcart.json'
 import guiaCastanhal from './guia-castanhal.json'
 import labprice from './labprice.json'
+import testAdopet from './test-adopet.json'
+import organoTestRobot from './organo-test-robot.json'
 
-const projects = [estoqueTeste, bookcart, labprice, guiaCastanhal]
+const projects = [estoqueTeste, bookcart, labprice, guiaCastanhal, testAdopet, organoTestRobot]
 
 export default projects
