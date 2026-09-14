b11c60c74262f2c9395a28850e58155c318fa80c
Ali-Maia <aliciaengcomp@gmail.com>
2026-09-14 14:30:18 -0300

    feat: adiciona badge introduction to cybersecurity
    Adiciona o emblema Cisco Introduction to Cybersecurity à lista de badges exibida na seção de cibersegurança.

--- src/data/cybersecurity.js
+++ src/data/cybersecurity.js
@@ -11,6 +11,12 @@ const cybersecurityBadges = [
     image: 'https://images.credly.com/size/340x340/images/88316fe8-5651-4e61-a6be-5be1558f049e/image.png',
     url: 'https://www.credly.com/badges/cdb4e9ca-4c19-48cc-a8d0-0afab8d9a0a4',
   },
+  {
+    name: 'Introduction to Cybersecurity',
+    issuer: 'Cisco',
+    image: 'https://images.credly.com/size/340x340/images/af8c6b4e-fc31-47c4-8dcb-eb7a2065dc5b/I2CS__1_.png',
+    url: 'https://www.credly.com/badges/3c88ddf5-4cdc-4e5e-a4c9-d6c6285b9b1e',
+  },
 ]
 
 export default cybersecurityBadges
